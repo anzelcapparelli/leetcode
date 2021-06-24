0f4cf0e5b8189bd95eb7c5c6dd381a7ce9421d97
Anzel Capparelli <anzelcapparelli@gmail.com>
2021-06-24 09:50:23 -0700

    6 initial solution

--- 6zigzagConvert.js
+++ 6zigzagConvert.js
@@ -4,42 +4,38 @@
  * @return {string}
  */
 var convert = function (s, numRows) {
-    var down = true;
-    // answr = new Array(numRows).fill([])
-    // ^^^ doesn't seem to work: a change to any array is reflected in all 
-    var answr = [];
-    var strAnswr = "";
-    for (i = 0; i < numRows; ++i) {
-        answr.push([])
+
+    if (numRows === 1) {
+        return s;
     }
 
+    var down = true;
+    var answr = new Array(numRows).fill("")
+    var strAnswr = "";
+
+
     for (i = 0, count = -1, l = s.length; i < l; ++i) {
-        if (numRows === 1) {
-            answr[i]=(s)
-            console.log(answr)
-        } else if (down) {
+        if (down) {
             if (count < numRows - 1) {
                 count++;
-                answr[count].push(s[i]);
+                answr[count]+=s[i];
             } else {
                 down = false;
                 i--;
             }
-            console.log(answr)
         } else {
             if (count > 0) {
                 count--;
-                answr[count].push(s[i]);
+                answr[count]+=s[i];
             } else {
                 down = true;
                 i--;
             }
-            console.log(answr)
         }
     }
 
     for (i = 0; i < numRows; ++i) {
-        strAnswr += answr[i].join("")
+        strAnswr += answr[i]
     }
 
     return strAnswr
@@ -47,12 +43,12 @@ var convert = function (s, numRows) {
 
 
 
-// var s = "PAYPALISHIRING";
-// var numRows = 4;
+var s = "PAYPALISHIRING";
+var numRows = 4;
 // Output: "PINALSIGYAHRPI"
 
-var s = "ABC";
-var numRows = 1;
+// var s = "ABC";
+// var numRows = 1;
 // Output: "AB"
 
 console.log(convert(s, numRows))
